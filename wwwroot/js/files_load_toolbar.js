b9820fb54e996d06fbe1d1c229f0d3fa8df79d88
timur <timtimch123@gmail.com>
2026-07-18 00:17:29 +0300

    Add client-side library and toolbar for dynamic file loading

---
+++ wwwroot/js/files_load_toolbar.js
@@ -0,0 +1,107 @@
+'use strict';
+
+(function() {
+    const t = ultra.tags;
+
+    let files_state = new ultra.State([], 'files', {
+        auto_notify: false,
+        render_callback: render,
+        after_callback: touch_input_tag
+    });
+    let id_counter = 0;
+
+    function revoke_url(obj) {
+        if (obj.url) {
+            URL.revokeObjectURL(obj.url);
+            obj.url = null;
+        }
+    }
+
+    function render(el, files) {
+        el.$clear();
+
+        if (files.length === 0) {
+            el.$replace_all(t.span('Файлы не выбраны.').$class('empty-message'));
+            return;
+        }
+
+        for (const item of files) {
+            const card = t.div(
+                () => {
+                    let child;
+                    if (item.file.type.startsWith('image/')) {
+                        child = t.img(item.url);
+                    } else {
+                        child = t.span('🗋').$style('fontSize', '40px');
+                    }
+                    return t.div(child).$class('preview');
+                },
+                t.button('×')
+                    .$class('remove-btn')
+                    .$attr('aria-label', 'Удалить файл')
+                    .$onclick(() => {
+                        const id = item.id;
+                        remove_file_by_id(id);
+                    }),
+            ).$class('card');
+
+            el.$append(card);
+        }
+    }
+
+    function add_files(new_files) {
+        if (!new_files || new_files.length === 0) return;
+
+        const new_items = [];
+        for (const file of new_files) {
+            const url = URL.createObjectURL(file);
+            new_items.push({
+                id: id_counter++,
+                file: file,
+                url: url,
+            });
+        }
+
+        files_state.val.splice(0, 0, ...new_items);
+        files_state.notify();
+    }
+
+    // Update input tag for change event to work when loading same file after removing from list
+    // We have files state fully in javascript inside files_state variable, and input tag doesn't know when we change the state
+    function touch_input_tag() {
+        file_input.value = [];
+    }
+
+    function remove_file_by_id(id) {
+        const index = files_state.val.findIndex(it => it.id === id);
+        if (index === -1) return;
+        revoke_url(files_state.val[index]);
+        files_state.val.splice(index, 1);
+        files_state.notify();
+    }
+
+    const file_input = ultra.by_id('images');
+    ultra.from_dom(file_input.closest('form.form')).$on_submit(() => {
+        file_input.value = files_state.val.map(o => o.file);
+    });
+
+    ultra.by_id('add-files').$on_click(() => {
+        file_input.click();
+    });
+
+    ultra.by_id('clear-files').$on_click(() => {
+        files_state.val.forEach(revoke_url);
+        files_state.val = [];
+        files_state.notify();
+    });
+
+    file_input.$on_change(e => {
+        e.preventDefault();
+        const selected_files = e.target.files;
+        if (selected_files && selected_files.length > 0) {
+            add_files(selected_files);
+        }
+    });
+
+    files_state.notify();
+})()
---
+++ wwwroot/js/ultra.js
@@ -0,0 +1,342 @@
+const ultra = (function() {
+    'use strict';
+    const ultra = {
+        tag: null,
+        tags: {},
+        from_dom: null,
+        by_id: null,
+        by_selector: null,
+        State: null,
+        get_dom_element: null,
+    };
+
+    ultra.tag = tag;
+    ultra.from_dom = from_dom;
+    ultra.dump_tags_to_window = dump_tags_to_window;
+    ultra.by_id = by_id;
+    ultra.by_selector = by_selector;
+    ultra.get_dom_element = get_dom_element;
+
+    function tag(name, ...children) {
+        const element = from_dom(document.createElement(name));
+        element.$append(...children);
+        return element;
+    }
+
+    const tag_handler = {
+        set(target, prop, value) {
+            target[prop] = value;
+            return true;
+        },
+
+        get(target, prop, receiver) {
+            if (!prop.startsWith('$')) {
+                let field = target[prop];
+                if (typeof(field) === 'function') {
+                    return (...args) => field.call(target, ...args);
+                }
+                return field;
+            }
+
+            // Native Node object of the DOM
+            if (prop === '$node') {
+                return target;
+            }
+
+            if (prop === '$attr') {
+                return (name, value) => {
+                    if (value === undefined) value = true; // Assuming boolean attribute
+                    target.setAttribute(name, value)
+                    return receiver;
+                };
+            }
+
+            const event_prefix = '$on';
+            if (prop.startsWith(event_prefix)) {
+                let event_name = prop.slice(event_prefix.length);
+                if (event_name[0] === '_') event_name = event_name.slice(1);
+                return (callback, ...args) => {
+                    target.addEventListener(event_name, callback, ...args);
+                    return receiver;
+                };
+            }
+
+            const class_prefix = '$class';
+            if (prop.startsWith(class_prefix)) {
+                const command = prop.slice(class_prefix.length+1);
+                if (command === '' || command === 'add') {
+                    return (...classes) => {
+                        target.classList.add(...classes);
+                        return receiver;
+                    };
+                } else if (command === 'remove') {
+                    return (...classes) => {
+                        target.classList.remove(...classes);
+                        return receiver;
+                    };
+                } else if (command === 'toggle') {
+                    return (class_name, cond) => {
+                        target.classList.toggle(class_name, cond);
+                        return receiver;
+                    };
+                } else if (command === 'replace') {
+                    return (class_to_replace, class_replace_with) => {
+                        target.classList.replace(class_to_replace, class_replace_with);
+                        return receiver;
+                    };
+                }
+
+                throw new Exception('Unexpected classList command: ' + command);
+            }
+
+            if (prop === '$style') {
+                return (name, value) => {
+                    target.style[name] = value;
+                    return receiver;
+                };
+            }
+
+            const data_prefix = '$data';
+            if (prop.startsWith(data_prefix)) {
+                const command = prop.slice(data_prefix.length+1);
+                return (name, value) => {
+                    if (command === '' || command === 'set') {
+                        target.dataset[name] = value;
+                    } else if (command === 'unset') {
+                        delete target.dataset[name];
+                    }
+                    return receiver;
+                };
+            }
+
+            if (prop === '$by_id') {
+                return (id) => {
+                    return by_id(id, target);
+                };
+            }
+
+            if (prop === '$by_selector') {
+                return (selector) => {
+                    return by_selector(selector, target);
+                };
+            }
+
+            if (prop === '$append') {
+                return (...children) => {
+                    for (const child of children) {
+                        target.appendChild(get_dom_element(child));
+                    }
+                    return receiver;
+                };
+            }
+
+            if (prop === '$prepend') {
+                return (...children) => {
+                    for (const child of children) {
+                        target.prepend(get_dom_element(child));
+                    }
+                    return receiver;
+                };
+            }
+
+            if (prop === '$replace') {
+                return (node, child) => {
+                    target.replaceChild(get_dom_element(node), get_dom_element(child));
+                    return receiver;
+                };
+            }
+
+            if (prop === '$replace_all') {
+                return (...children) => {
+                    target.replaceChildren(...children.map(get_dom_element));
+                    return receiver;
+                };
+            }
+
+            if (prop === '$clear') {
+                return () => {
+                    target.replaceChildren();
+                    return receiver;
+                };
+            }
+
+            throw new Exception('Unexpected member: ' + prop);
+        },
+    };
+
+    function from_dom(element) {
+        if (element.$node) return element;
+        return new Proxy(element, tag_handler);
+    }
+
+    function get_dom_element(obj) {
+        if (typeof(obj) === 'string') {
+            return document.createTextNode(obj);
+        } else if (typeof(obj) === 'function') {
+            return get_dom_element(obj());
+        } else if (obj.$node) {
+            return obj.$node;
+        } else {
+            return obj;
+        }
+    }
+
+    function by_id(id, root = document) {
+        const element = root.getElementById(id);
+        return from_dom(element);
+    }
+
+    function by_selector(selector, root = document) {
+        const element = root.querySelector(selector);
+        return from_dom(element);
+    }
+
+    function img(src, alt) {
+        if (!alt) alt = '';
+        return tag('img').$attr('src', src).$attr('alt', alt);
+    }
+
+    function input(type) {
+        return tag('input').$attr('type', type);
+    }
+
+    class State {
+        #val = '';
+        #subs = [];
+        #name = '';
+        auto_notify = true;
+        update_callback = null;
+        after_callback = null;
+        before_callback = null;
+
+        constructor(initial_state, name, { auto_notify = true, update_callback = null, after_callback = null, before_callback = null }) {
+            this.#val = initial_state;
+            if (update_callback) {
+                this.update_callback = update_callback;
+            } else {
+                this.update_callback = (el, state) => el.textContent = state.toString();
+            }
+            this.after_callback = after_callback;
+            this.before_callback = before_callback;
+            this.#name = name;
+            this.auto_notify = auto_notify;
+            this.resubscribe_all();
+            if (this.auto_notify) this.notify();
+        }
+
+        set val(state) {
+            if (state !== this.#val) {
+                this.#val = state;
+                if (this.auto_notify) this.notify();
+            }
+        }
+
+        get val() {
+            return this.#val;
+        }
+
+        get name() {
+            return this.#name;
+        }
+
+        subscribe(element, update_callback = null) {
+            if (element.dataset.state === this.#name) this.subscribe1(element);
+            const children = element.querySelectorAll(`[data-state="${this.#name}"]`);
+            for (const child of children) {
+                this.subscribe1(child, update_callback);
+            }
+        }
+
+        subscribe1(element, update_callback = null) {
+            this.#subs.push({
+                element: from_dom(element),
+                update: update_callback,
+            });
+        }
+
+        unsubscribe(element) {
+            const index = this.#subs.findIndex((val) => val.element === element);
+            this.#subs.splice(index, 1);
+        }
+
+        input_event_func() {
+            return (e) => {
+                if (e.target.type === 'number') {
+                    this.val = Number(e.target.value);
+                } else {
+                    this.val = e.target.value;
+                }
+            };
+        }
+
+        unsubscribe_all() {
+            for (const sub of this.#subs) {
+                if (sub.dataset && sub.dataset.bind === this.#name) {
+                    sub.removeEventListener('input', this.input_event_func());
+                }
+            }
+            this.#subs = [];
+        }
+
+        resubscribe_all(parent = document) {
+            this.#subs = [];
+            let new_subs_els = parent.querySelectorAll(`[data-state="${this.#name}"]`);
+            for (const el of new_subs_els) {
+                this.subscribe(el);
+            }
+            this.bind_all(parent);
+        }
+
+        bind_all(parent = document) {
+            const elements = parent.querySelectorAll(`input[data-bind="${this.#name}"]`);
+            for (const e of elements) {
+                e.addEventListener('input', this.input_event_func());
+                this.subscribe1(e, (el, state) => el.value = state);
+            }
+        }
+
+        notify() {
+            if (this.before_callback) this.before_callback(this.#val);
+            for (const sub of this.#subs) {
+                if (sub.update) {
+                    sub.update(sub.element, this.#val);
+                } else {
+                    this.update_callback(sub.element, this.#val);
+                }
+            }
+            if (this.after_callback) this.after_callback(this.#val);
+        }
+    }
+    ultra.State = State;
+
+    const PREDEFINED_TAGS = [
+        'div', 'span',
+        'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'blockquote', 'pre',
+        'a', 'abbr', 'acronym', 'address', 'big', 'cite', 'code',
+        'del', 'dfn', 'em', 'ins', 'kbd', 'q', 's', 'samp',
+        'small', 'strike', 'strong', 'sub', 'sup', 'tt', 'var',
+        'b', 'u', 'i', 'center',
+        'dl', 'dt', 'dd', 'ol', 'ul', 'li',
+        'table', 'caption', 'tbody', 'tfoot', 'thead', 'tr', 'th', 'td',
+        'article', 'aside', 'canvas', 'details', 'embed',
+        'figure', 'figcaption', 'footer', 'header', 'hgroup',
+        'menu', 'nav', 'output', 'ruby', 'section', 'summary',
+        'time', 'mark', 'audio', 'video',
+        'button',
+    ];
+    function add_tags(object) {
+        for (const tag_name of PREDEFINED_TAGS) {
+            object[tag_name] = (...children) => tag(tag_name, ...children);
+        }
+        object.img = img;
+        object.input = input;
+    }
+
+    add_tags(ultra.tags);
+
+    function dump_tags_to_window() {
+        add_tags(window);
+    }
+
+    return ultra;
+})();
